81fb6f7982cee901701703563d2c20d03a0a17bf
Bryan Schoot <Bryan Schoot>
2017-09-20 20:32:56 +0200

    Laravel: changed laravel version to 5.5.* and php to 7.1.1

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Ultrawares
+//        Ultraware\Roles\RolesServiceProvider::class,
     ],
 
     /*
