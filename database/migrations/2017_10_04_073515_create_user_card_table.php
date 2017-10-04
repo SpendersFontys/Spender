1d48c2006d5f318a95d95044fe49b8138f2450da
Nico <nico.g.shultz@gmail.com>
2017-10-04 10:37:47 +0200

    created migration for the database

---
+++ database/migrations/2017_10_04_073515_create_user_card_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserCardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_card', function (Blueprint $table) 
+        {
+            $table->increments('id');
+            $table->integer('card_id');
+            $table->foreign('card_id')->references('id')->on('card')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_card');
+    }
+}
